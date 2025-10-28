068bc05a296254bfc9bf1b285eaddce6dddacd57
NeaBouli <bergamolia@protonmail.com>
2025-10-28 08:56:44 +0100

    fix: restore MockERC20 and correct import paths

--- foundry/test/FeeRouter.t.sol
+++ foundry/test/FeeRouter.t.sol
@@ -5,6 +5,6 @@ import "forge-std/Test.sol";
 import {FeeRouter} from "../../contracts/core/FeeRouter.sol";
 import {IFeeRouter} from "../../contracts/interfaces/IFeeRouter.sol";
 import {TreasuryVault} from "../../contracts/core/TreasuryVault.sol";
-import {MockERC20} from "../mocks/MockERC20.sol";
+import {MockERC20} from "../../mocks/MockERC20.sol";
 
 // ... Rest des Tests unverändert
--- foundry/test/TreasuryVault.t.sol
+++ foundry/test/TreasuryVault.t.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import {TreasuryVault} from "../../contracts/core/TreasuryVault.sol";
-import {MockERC20} from "../mocks/MockERC20.sol";
+import {MockERC20} from "../../mocks/MockERC20.sol";
 
 // ... Rest des Tests unverändert
