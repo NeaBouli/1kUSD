c03393d737e750570cc96fa914b4a8caa0521ba3
NeaBouli <bergamolia@protonmail.com>
2025-11-11 20:21:32 +0100

    test(dev41): initialize oracle regression test suite

---
+++ foundry/test/oracle/OracleRegression_Base.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import "forge-std/Test.sol";
+import "../../contracts/oracle/OracleWatcher.sol";
+import "../../contracts/core/OracleAggregator.sol";
+import "../../contracts/core/SafetyAutomata.sol";
+
+contract OracleRegression_Base is Test {
+    OracleWatcher watcher;
+    OracleAggregator aggregator;
+    SafetyAutomata safety;
+
+    function setUp() public {
+        safety = new SafetyAutomata(address(this), 0);
+        aggregator = new OracleAggregator(address(this), safety);
+        watcher = new OracleWatcher(aggregator, safety);
+    }
+}
