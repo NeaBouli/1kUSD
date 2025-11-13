9383a579aab4bc1c2a5c5a142070a3548352d796
NeaBouli <bergamolia@protonmail.com>
2025-11-13 09:45:47 +0100

    fix(dev41-t43): make OracleRegression_Watcher inherit OracleRegression_Base and remove shadowing fields

--- foundry/test/oracle/OracleRegression_Watcher.t.sol
+++ foundry/test/oracle/OracleRegression_Watcher.t.sol
@@ -7,11 +7,8 @@ import "contracts/core/SafetyAutomata.sol";
 import "contracts/core/OracleAggregator.sol";
 import "contracts/interfaces/IParameterRegistry.sol";
 import "./OracleRegression_Base.t.sol";
-contract OracleRegression_Watcher is Test {
+contract OracleRegression_Watcher is OracleRegression_Base {
     OracleWatcher watcher;
-    SafetyAutomata safety;
-    OracleAggregator aggregator;
-    IParameterRegistry registry;
     event HealthUpdated(IOracleWatcher.Status status, uint256 timestamp);
     function setUp() public override {
         super.setUp();
