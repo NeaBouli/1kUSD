509f2944b7f998954e72bb8b96e907daefc1e5b5
NeaBouli <bergamolia@protonmail.com>
2025-11-16 20:48:58 +0100

    dev45: add MockERC20 for PSM flow tests

---
+++ foundry/test/mocks/MockERC20.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor(string memory n, string memory s) ERC20(n, s) {}
+    function mint(address to, uint256 a) external { _mint(to, a); }
+}
